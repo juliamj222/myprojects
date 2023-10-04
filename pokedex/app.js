3ebe9d613046632f0e8b6dac91ec2ac577100c84
julia menendez jardon <juliamenendezjardon@gmail.com>
2023-10-04 16:55:26 -0400

    pokédex, working name and weight

--- pokedex/app.js
+++ pokedex/app.js
@@ -1,15 +1,4 @@
-/* 
-    ? Instructions
-    * connect your files (index, style, app)
-    * create a pokedex card in index: (div)
-    * it should have name (h3), weight (p), and picture (img)
-    * above it, there should be a form with an input field and a Go button
-    * access all elements within app.js using DOM
-    * create an event listener that listens for the button click
-    * when the button is clicked, it should grab the input value (aka pokemon name)
-    * it should then trigger a fetch to the pokemon api with that pokemon name
-    * it should then display the values of that pokemon's name, weight, and picture
-*/
+//? Pokédex
 const url="https://pokeapi.co/api/v2/pokemon/"
 
 const pokeName = document.getElementById("pokeName");
@@ -22,7 +11,7 @@ const capitalize=str=>{
     return str[0].toUpperCase()+str.slice(1).toLowerCase()
 }
 
-const mainContainer = document.getElementById("pokeDex");
+const mainContainer = document.getElementById("pokedex");
 
 const renderError = () => {
     card.style.display = "none";
@@ -31,13 +20,17 @@ const renderError = () => {
     mainContainer.appendChild(h2);
 }
 
-
 const render = (data) => {
     console.log(data);
     pokeName.textContent = `Name: ${capitalize(data.name)}`;
     pokeWeight.textContent = `Weight: ${data.weight}`;
     pokePic.src = data.sprites.front_shiny;
 }
+
+goButton.addEventListener("click", event => {
+    event.preventDefault();
+    getData(event.target.form[0].value);
+    })
 
 const getData=async(pokemon) => {
 
@@ -49,9 +42,4 @@ const getData=async(pokemon) => {
     console.log(err)
     renderError()
 }
-
-goButton.addEventListener("click", event => {
-event.preventDefault()
-getData(event.target.form[0].value)
-})
 }
